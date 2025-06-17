2421d4589788c773af58fa4d57544d60c8123642
puckaua <puckaua@gmail.com>
2025-06-17 12:01:52 -0300

    Add files via upload

---
+++ exercicio01.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio01
+{
+
+    internal class Cliente
+    {
+        private string nome;
+        private string endereco;
+        private int telefone;
+
+        public Cliente(string nome, string endereco)
+        {
+            this.nome = nome;
+            this.endereco = endereco;
+        }
+
+        public Cliente(string nome, string endereco, int telefone)
+        {
+            this.nome = nome;
+            this.endereco = endereco;
+            this.telefone = telefone;
+        }
+
+        public string Nome 
+        {
+            get { return nome; } 
+            set { nome = value; }
+        }
+
+        public string Endereco
+        {
+            get { return endereco; }
+            set { endereco = value; }
+        }
+
+        public int Telefone
+        {
+            get { return telefone; }
+            set { telefone = value; }
+        }
+
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputNome, inputEndereco; int inputTelefone;
+            Cliente[] clientes = new Cliente[3];
+            for (int i = 0; i < clientes.Length; i++)
+            {
+                Console.WriteLine($"Digite o nome do cliente {i + 1}");
+                inputNome = Console.ReadLine();
+                Console.WriteLine($"Digite o telefone do cliente {i + 1}");
+                inputTelefone = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Digite o endereço do cliente {i + 1}");
+                inputEndereco = Console.ReadLine();
+                clientes[i] = new Cliente(inputNome, inputEndereco);
+                clientes[i].Telefone = inputTelefone;
+
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < clientes.Length; i++)
+            {
+                Console.WriteLine($"Cliente {i+1}:");
+                Console.WriteLine(clientes[i].Nome);
+                Console.WriteLine(clientes[i].Telefone);
+                Console.WriteLine(clientes[i].Endereco);
+                Console.WriteLine();
+
+
+            }
+        }
+    }
+}
---
+++ exercicio02.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio02
+{
+    class Data
+    {
+        private int dia;
+        private int mes;
+        private int ano;
+
+
+        public Data(int dia, int mes, int ano)
+        {
+            this.dia = dia;
+            this.mes = mes;
+            this.ano = ano;
+        }
+
+        public string ToString()
+        {
+            string diaString = $"{this.dia.ToString()}/";
+            string mesString = $"{this.mes.ToString()}/";
+            string anoString = $"{this.ano.ToString()}";
+            string resultado = diaString + mesString + anoString ;
+            return resultado ;
+
+        }
+
+        public int Dia
+        {
+            get { return this.dia; }
+            set { this.dia = value; }
+        }
+
+        public int Mes
+        {
+            get { return this.mes; }
+            set { this.mes = value; }
+        }
+
+        public int Ano
+        {
+            get { return this.ano; }
+            set { this.ano = value; }
+        }
+
+    }
+        
+    internal class Teste
+    {
+        
+        static void Main(string[] args)
+        {
+            int dia, mes, ano;
+            string resultado;
+            Console.WriteLine("Digite o dia: ");
+            dia = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o mês: ");
+            mes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o ano: ");
+            ano = int.Parse(Console.ReadLine());
+
+            Data d1 = new Data(dia, mes, ano);
+            d1.Dia = dia + 1;
+            resultado = d1.ToString();
+
+            Console.WriteLine("Resultado:" + resultado);
+
+            Console.WriteLine("Digite o dia: ");
+            dia = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o mês: ");
+            mes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o ano: ");
+            ano = int.Parse(Console.ReadLine());
+
+            Data d2 = new Data(dia, mes, ano);
+            d2.Dia = dia + 1;
+            resultado = d2.ToString();
+
+            Console.WriteLine("Resultado:" + resultado);
+
+        }
+    }
+}
